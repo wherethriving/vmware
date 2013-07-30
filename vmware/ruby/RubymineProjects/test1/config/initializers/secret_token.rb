49468286163804f29e6fa3bdab94234afac7aa0e
wherethriving <wherethriving@hotmail.com>
2013-07-30 14:03:13 +0800

    test sync with windows

---
+++ vmware/ruby/RubymineProjects/test1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test1::Application.config.secret_token = '06631040acf3830d6ecf34371060c3f0a947bc93b9aad1bdbed51f31fc1f434e0ae5f93e2e9a0068c49e8e67b781e67f030619a12bc2542980cd769c97539af1'
